dbe906bfbcc0b71f9ac4d2b74e6247252a6c2854
Abhijeet7533 <abhigovekar7533@gmail.com>
2025-02-11 09:40:56 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,4 +1,10 @@
 a=int(input("Enter 1st No="))
 b=int(input("Enter 2nd No="))
-ans=a+b
-print("Addition of Numbers",ans)
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition of Numbers",add)
+print("substraction of Numbers",sub)
+print("multiplication of Numbers",mul)
+print("division of Numbers",div)
